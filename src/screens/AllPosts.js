3d2b30af307dc0ad003da495565fc66a8d2a1022
Felipe Navas <felipenavas.itec@gmail.com>
2022-06-23 21:15:12 -0300

    Adding API to get posts

--- src/screens/AllPosts.js
+++ src/screens/AllPosts.js
@@ -1,74 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const AllPosts = () => {
+  const [posts, setPosts] = useState([])
+  const [loader, setLoader] = useState(false)
+
+  const getAllPosts = () => {
+    setLoader(true)
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/posts`)
+      .then((response) => response.json())
+      .then((json) => {
+        setPosts(json)
+        setLoader(false)
+      })
+  }
+
+  useEffect(() => {
+    getAllPosts()
+  }, [])
+
   return (
     <div>
       <section className="container pt-2">
         <h3 className="text-center text-uppercase py-4">All Posts</h3>
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-12">
-            <div className="card">
-              <img
-                src="https://images.unsplash.com/photo-1582063289852-62e3ba2747f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="d-grid">
-                  <a href="/" className="btn btn-warning">
-                    Read More
-                  </a>
-                </div>
+          {loader ? (
+            <div className='col-12 text-center'>
+              <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
               </div>
             </div>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-12">
-            <div className="card">
-              <img
-                src="https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="d-grid">
-                  <a href="/" className="btn btn-warning">
-                    Read More
-                  </a>
+          ) : (
+            posts.map((post, index) => {
+              return (
+                <div key={index} className="col-lg-4 col-md-4 col-sm-12 mt-2">
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/random/400*400/?city,night"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{post.title}</h5>
+                      <p className="card-text">{post.body}</p>
+                      <div className="d-grid">
+                        <a href="/" className="btn btn-warning">
+                          Read More
+                        </a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-4 col-sm-12">
-            <div className="card">
-              <img
-                src="https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <div className="d-grid">
-                  <a href="/" className="btn btn-warning">
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+              )
+            })
+          )}
         </div>
       </section>
     </div>
